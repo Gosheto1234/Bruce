f166a242a2103b2770d2ab2552f69c355f542808
Gosheto1234 <104501999+Gosheto1234@users.noreply.github.com>
2025-05-28 05:24:09 -0700

    Update settings_UIColors.h

--- src/core/settings_UIColors.h
+++ src/core/settings_UIColors.h
@@ -1,1 +1,14 @@
-
+#define DARK_GREY 0x1082
+#define Eagle 0xB591
+#define Celadon 0xA7B6
+#define Tropical_Blue 0xA61D
+#define Pear 0xD7A6
+#define Shocking_Pink 0xDD39
+#define Old_Rose 0xC189
+#define Olive_Green 0xBD8A
+#define Violet 0xAAD4
+#define Midnight_Blue 0x190C
+#define Purple 0x594F
+#define Alizarin 0xE8E7
+#define Finn 0x69CA
+// add more RGB565 format colors here:
